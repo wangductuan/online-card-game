5ae455a1f63ff29e7964c05e337f3e565ea1bfdf
wangductuan <wangductuan@icloud.com>
2025-05-02 19:21:18 +0700

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-app.js";
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/10.11.0/firebase-database.js";
 
